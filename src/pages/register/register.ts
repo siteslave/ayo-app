83a7dc5a67635e22f4fcd8fe651537515602009b
Satit Riapit <siteslave@msn.com>
2018-04-25 15:37:11 +0700

    add sqlite

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { SQLiteObject, SQLite } from '@ionic-native/sqlite';
 
 @IonicPage()
 @Component({
@@ -9,12 +10,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class RegisterPage {
 
   cid: any;
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private platform: Platform,
+    private sqlite: SQLite
+  ) { }
 
   doRegister() {
+    this.platform.ready().then(() => {
 
+      this.sqlite.create({
+        name: 'drugnotify.db',
+        location: 'default'
+      })
+        .then((db: SQLiteObject) => {
+
+          let sql = `
+              CREATE TABLE IF NOT EXISTS profile(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                cid TEXT,
+                hn TEXT,
+                hospcode TEXT,
+                url TEXT)
+            `;
+
+          db.executeSql(sql, {})
+            .then(() => console.log('Executed SQL'))
+            .catch(e => console.log(e));
+
+
+        })
+        .catch(e => console.log(e));
+    });
   }
 
 }
