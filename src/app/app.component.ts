6eecf7da58c46e22eecc15dc980302f7d85e4282
Satit Riapit <siteslave@msn.com>
2018-04-26 11:46:09 +0700

    add profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,7 @@ export class MyApp {
         location: 'default'
       })
         .then((db: SQLiteObject) => {
-          let sqlDelete = 'DROP TABLE IF EXISTS profile;';
+          let sqlDelete = 'SELECT 1 as t;';
           let sql = `
               CREATE TABLE IF NOT EXISTS profile(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
