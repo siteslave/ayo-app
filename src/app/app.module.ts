8d3e39f299e9ecf3df5324ba1954c4bdfbc6d047
Satit Riapit <siteslave@msn.com>
2018-04-27 12:35:11 +0700

    add push notify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -51,7 +52,8 @@ import { DrugsPageModule } from '../pages/drugs/drugs.module';
     LoginProvider,
     SQLite,
     BarcodeScanner,
-    NotifyProvider
+    NotifyProvider,
+    LocalNotifications
   ]
 })
 export class AppModule { }
--- src/pages/drugs/drugs.ts
+++ src/pages/drugs/drugs.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams, Platform, AlertController } from '
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { SQLite } from '@ionic-native/sqlite';
 import { NotifyProvider } from '../../providers/notify/notify';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 @IonicPage()
 @Component({
@@ -10,7 +11,7 @@ import { NotifyProvider } from '../../providers/notify/notify';
   templateUrl: 'drugs.html',
 })
 export class DrugsPage {
-
+  drugs: any = [];
   hn: any;
   url: any;
   hospcode: any;
@@ -23,7 +24,8 @@ export class DrugsPage {
     private platform: Platform,
     private sqlite: SQLite,
     private alertCtrl: AlertController,
-    private notifyProvider: NotifyProvider
+    private notifyProvider: NotifyProvider,
+    private localNotifications: LocalNotifications
   ) {
     this.hn = this.navParams.get('hn');
     // this.url = this.navParams.get('url');
@@ -32,7 +34,14 @@ export class DrugsPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DrugsPage');
+    
+    this.localNotifications.schedule({
+      id: 1,
+      text: 'แจ้งเตือนการกินยา',
+      vibrate: true,
+      trigger: { at: new Date(new Date().getTime() + 5000) },
+    });
+
   }
 
   doScan() {
@@ -44,8 +53,9 @@ export class DrugsPage {
               console.log(res);
               if (res.ok) {
                 // console.log(res.rows);
+                this.drugs = res.rows;
               } else {
-
+                // error
               }
             }, (error: any) => {
               console.error(error);
